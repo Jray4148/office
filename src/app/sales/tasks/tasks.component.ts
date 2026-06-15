04894f07ea55306d59d052dcf199285099ae29fa
Jray4148 <jesse@roughnecksoftware.com>
2026-06-15 14:38:13 -0500

    getting tasks back

--- src/app/sales/tasks/tasks.component.ts
+++ src/app/sales/tasks/tasks.component.ts
@@ -2,10 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { TasksService } from '@/services/tasks.service';
 import { firstValueFrom } from 'rxjs';
 import {TasksResponse} from "@/types/tasks-reponse";
+import {FilterActionToolbarComponent} from "@/custom/filter-action-toolbar/filter-action-toolbar.component";
+import {CurrencyPipe, DatePipe} from "@angular/common";
+import {PrimeTemplate} from "primeng/api";
+import {TableModule} from "primeng/table";
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [
+    FilterActionToolbarComponent,
+    CurrencyPipe,
+    DatePipe,
+    PrimeTemplate,
+    TableModule
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
@@ -18,5 +28,6 @@ export class TasksComponent implements OnInit {
 
   async ngOnInit() {
     this.tableData = await firstValueFrom(this.tasksService.getTasks());
+    console.log(this.tableData);
   }
 }
